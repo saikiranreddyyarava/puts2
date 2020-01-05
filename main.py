e648466079c474992f872452012fe9b6acf0034b
saikiranreddyyarava <kiranreddyyarava@gmail.com>
2020-01-05 23:33:27 +0100

    div added

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from flask import Flask, request
-
+from fractions import Fraction
+from decimal import Decimal
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
-
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
+@app.route('/div')
+def division():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        input1= Fraction(value1)
+        input2= Fraction(value2)
+        try:
+            output= input1/input2
+            return(str(round(float(output),3)))
+        except ZeroDivisionError as error:
+            return 'None'
 
 if __name__ == "__main__":
     app.run()
