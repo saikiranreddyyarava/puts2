79052ec33fbadd943085bbfd4d17c3d774bf40d6
saikiranreddyyarava <kiranreddyyarava@gmail.com>
2020-05-30 15:01:55 +0200

    changes are made in subtraction branch for the accurate output

--- test.py
+++ test.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python3
+
 import main
 import unittest
 
 class MyTestCase(unittest.TestCase):
 
-	def setUp(self):
+
+        def setUp(self):
             main.app.testing = True
             self.app = main.app.test_client()
-
-	def test_subint(self):
-            valuesread =  self.app.get('/sub?A=9&B=13')
-            self.assertEqual(b'-4.0', valuesread.data)
+        
+        def test_subint(self):
+            valuesread =  self.app.get('/sub?A=589&B=492')
+            self.assertEqual(b'97', valuesread.data.strip())
         def test_subfloat(self):
-            valuesread =  self.app.get('/sub?A=4.3&B=9.8')
-            self.assertEqual(b'-5.5', valuesread.data)
+            valuesread =  self.app.get('/sub?A=8.2&B=15.6')
+            self.assertEqual(b'-7.4', valuesread.data.strip())
         def test_subfrac(self):
-            valuesread =  self.app.get('/sub?A=8/3&B=9/2')
-            self.assertEqual(b'-1.833', valuesread.data)
+            valuesread =  self.app.get('/sub?A=9/5&B=18/2')
+            self.assertEqual(b'-7.2', valuesread.data.strip())
         def test_subneg(self):
-            valuesread =  self.app.get('/sub?A=6.4&B=-9.3')
-            self.assertEqual(b'2.9', valuesread.data)
+            valuesread =  self.app.get('/sub?A=95.4&B=-13.1')
+            self.assertEqual(b'108.5', valuesread.data.strip())
+
 
 if __name__ == '__main__':
     unittest.main()
