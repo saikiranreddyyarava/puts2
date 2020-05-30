33d7739729aeb2e202345095d790994d21203b18
saikiranreddyyarava <kiranreddyyarava@gmail.com>
2020-05-30 14:41:33 +0200

    modified Addition branch for the accurate output

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-from flask import Flask, request
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
 from fractions import Fraction
 from decimal import Decimal
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
+def check_value(num):
+    value = 0
+    #print("number: {}, type: {}".format(num, type(num)))
+    try:
+        value = int(num.strip())
+        return value, "OK"
+    except ValueError as e:
+        try:
+            val = float(Fraction(num))
+            return val, "OK"
+        except ValueError as e:
+            return 0, "ERROR"
+        except ZeroDivisionError as e:
+            return 0, "ERROR"
+
+
 @app.route('/add')
 def addition():
-    try:
-        value1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value1='None'
-    try:
-        value2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value2='None'
-    if value1 == 'None' or value2 == 'None' :
-        return 'None'
-    else:
-        input1= Fraction(value1)
-        input2= Fraction(value2)
-        output= input1+input2
-        return str(round(float(output),3))
+    err = ""
+    value1 = request.args.get('A',default = 0)
+    value2 = request.args.get('B', default=0)
+    v1, err1 = check_value(value1)
+    v2, err2 = check_value(value2)
+    #print("debug: {} - {}\n{} - {}".format(v1, err1, v2, err2))
+    if err1 == "ERROR":
+        return jsonify("ERROR! invalid input"), 500
+    if err2 == "ERROR":
+        return jsonify("ERROR! invalid input"), 500
+    #print('{} + {} = '.format(value1, value2))
+    output= eval('{} + {}'.format(value1, value2))
+    return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run()
--- test.py
+++ test.py
@@ -1,24 +1,27 @@
+#!/usr/bin/python3
+
 import main
 import unittest
 
 class MyTestCase(unittest.TestCase):
+
 
         def setUp(self):
             main.app.testing = True
             self.app = main.app.test_client()
 
         def test_addint(self):
-            valuesread=  self.app.get('/add?A=4&B=9')
-            self.assertEqual(b'13.0', valuesread.data)
+            valuesread=  self.app.get('/add?A=9&B=69')
+            self.assertEqual(b'78', valuesread.data.strip())
         def test_addfloat(self):
-            valuesread=  self.app.get('/add?A=4.2&B=6.4')
-            self.assertEqual(b'10.6', valuesread.data)
+            valuesread=  self.app.get('/add?A=9.2&B=6.4')
+            self.assertEqual(b'15.6', valuesread.data.strip())
         def test_addfrac(self):
-            valuesread =  self.app.get('/add?A=6/2&B=8/3')
-            self.assertEqual(b'5.667', valuesread.data)
+            valuesread =  self.app.get('/add?A=5/2&B=9/4')
+            self.assertEqual(b'4.75', valuesread.data.strip())
         def test_addneg(self):
-            valuesread=  self.app.get('/add?A=6.2&B=-8.2')
-            self.assertEqual(b'-2.0', valuesread.data)
+            valuesread=  self.app.get('/add?A=15.1&B=-8.2')
+            self.assertEqual(b'6.9', valuesread.data.strip())
 
 if __name__ == '__main__':
     unittest.main()
